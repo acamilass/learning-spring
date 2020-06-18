e90682c7f57ad5d92dbf825dbc81033685e26198
Ana C. Santos Sousa <ana.c.santos.sousa@avanade.com>
2020-06-18 10:31:59 -0300

    configuration and bean

---
+++ src/main/java/sales/MyConfiguration.java
@@ -0,0 +1,13 @@
+package sales;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfiguration {
+
+    @Bean(name = "applicationName")
+    public String applicationName() {
+        return "Sales System";
+    }
+}
